25bee5b59bec4021864e49cf226006745e8f13e8
Pearl-Dsilva <pearlveola.dsilva@mca.christuniversity.in>
2024-08-09 10:43:23 +0530

    modified getbookings

--- discovery-service/discovery.py
+++ discovery-service/discovery.py
@@ -27,7 +27,7 @@ db = mongo.db.flights
 # This function will be invoked by Vendors/Admin
 # This function adds new flights to the database
 @app.route("/addflight", methods=["POST"])
-def create_item():
+def add_flight():
     data = request.json
     if "source" not in data or "destination" not in data or "date" not in data or "class" not in data or "available_seats" not in data:
         return jsonify({"message": "Invalid data"}), 400
@@ -38,52 +38,62 @@ def create_item():
     url = request.url
     # append /isadmin to the url and make a network request as to the admin status
     print(url)
+    # response = requests.get(url + "/isadmin")
 
-
-    # try:
-    #     result = db.insert_one({
-    #         "source": data["source"],
-    #         "destination": data["destination"],
-    #         "date": data["date"],
-    #         "class": data["class"],
-    #         "available_seats": data["available_seats"]
-    #     })  # Insert data into collection
-    #     return jsonify(
-    #         {"id": str(result.inserted_id), "source": data["source"], "destination": data["destination"], "date": data["date"], "class": data["class"], "available_seats": data["available_seats"]}
-    #     )  # Convert inserted_id to string
-    # except Exception as e:
-    #     return jsonify({"id": -1, "error": e.args[0]}), 500
-    
-
-    # if "username" not in data or "password" not in data:
-    #     return jsonify({"message": "Invalid data"}), 400
-
-    # try:
-    #     result = db.insert_one({
-    #         "username": data["username"],
-    #         "password": data["password"]
-    #     })  # Insert data into collection
-    #     return jsonify(
-    #         {"id": str(result.inserted_id), "name": data["username"]}
-    #     )  # Convert inserted_id to string
-    # except Exception as e:
-    #     return jsonify({"id": -1, "error": e.args[0]}), 500
+    try:
+        result = db.insert_one({
+            "source": data["source"],
+            "destination": data["destination"],
+            "date": data["date"],
+            "class": data["class"],
+            "available_seats": data["available_seats"]
+        })  # Insert data into collection
+        return jsonify(
+            {"id": str(result.inserted_id), "source": data["source"], "destination": data["destination"], "date": data["date"], "class": data["class"], "available_seats": data["available_seats"]}
+        )  # Convert inserted_id to string
+    except Exception as e:
+        return jsonify({"id": -1, "error": e.args[0]}), 500
 
 
 # This function will be invoked by the front end trying to get the available flights
 # here require the booking ID of the flight as this binds the flight booking to the flight details
-@app.route("/getBookings", methods=["GET"])
-def login_user():
-    data = request.json
-    # if "username" not in data or "password" not in data:
-    #     return jsonify({"message": "Invalid data"}), 400
-    
+@app.route("/getbookings", methods=["GET"])
+def show_booking():
     try:
-        user = db.find({"username": data["username"], "password": data["password"]})
-        if user:
-            return jsonify({"id": str(user["_id"]), "name": user["username"]}), 200
+        source = request.args.get("source")
+        destination = request.args.get("destination")
+        date = request.args.get("date")
+        flight_class = request.args.get("class")
+        available_seats = request.args.get("available_seats")
+
+        query = {}
+        if source:
+            query["source"] = source
+        if destination:
+            query["destination"] = destination
+        if date:
+            query["date"] = date
+        if flight_class:
+            query["class"] = flight_class
+        if available_seats:
+            query["available_seats"] = available_seats
+
+        flights = db.find(query)
+        if flights:
+            flight_list = []
+            for flight in flights:
+                flight_data = {
+                    "id": str(flight["_id"]),
+                    "source": flight["source"],
+                    "destination": flight["destination"],
+                    "date": flight["date"],
+                    "class": flight["class"],
+                    "available_seats": flight["available_seats"]
+                }
+                flight_list.append(flight_data)
+            return jsonify(flight_list), 200
         else:
-            return jsonify({"message": "User not found"}), 404
+            return jsonify({"message": "No flights found"}), 404
     except Exception as e:
         return jsonify({"id": -1, "error": e.args[0]}), 500
 
